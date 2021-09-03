51e98d8b54d7e83e7a36f98ce274ad0969df9e2f
Marcus Lira <marcuslira57@gmail.com>
2021-09-02 22:05:21 -0300

    update

---
+++ Projeto/__init__.py
@@ -0,0 +1,25 @@
+import tkinter
+
+
+class Application(tkinter.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tkinter.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tkinter.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tkinter.Tk()
+app = Application(master=root)
+app.mainloop()
